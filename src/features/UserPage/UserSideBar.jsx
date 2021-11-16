0fc9c14160b141e6e46aa43df9e382c445c19975
Minh Hieu <hieu0985792248@gmail.com>
2021-11-16 16:08:32 +0700

    userpage home

--- src/features/UserPage/UserSideBar.jsx
+++ src/features/UserPage/UserSideBar.jsx
@@ -1,37 +1,26 @@
 import { useState, useEffect } from 'react'
 import * as React from 'react'
-import '../Admin/sidebar.css';
-import { Reorder, People, ExitToApp, PersonAdd } from '@material-ui/icons';
+import './usersideBar.css';
+import { Reorder, People, ExitToApp, LibraryBooks } from '@material-ui/icons';
 import { Link } from 'react-router-dom';
 
 
 export default function UserSideBar() {
-    const [active, setactive] = useState(localStorage.getItem('store'));
 
-    useEffect(() => {
-        let b = document.getElementsByClassName('active');
-        for (let i = 0; i < b.length; i++) {
-            b[i].classList.remove('active');
-        }
-        if (active >= 1)
-            document.getElementById(active).classList.add('active');
-    }, [active])
 
     const liCkickFun = (event) => {
-        setactive(event.target.id);
-        localStorage.setItem('store', event.target.id);
+
     }
 
     return (
-        <div className="sidebar">
+        <div className="userSideBar">
             <div className="sideBarWrapper">
                 <div className="sideBarMenu">
                     <h3 className="sideBarTitle">DashBoard</h3>
                     <ul className="sideBarList">
-                        <Link to='/userPage' className="dirLink"><li id="1" className="sideBarListItem " onClick={(e) => liCkickFun(e)}><Reorder />Home</li></Link>
-                        <Link to='/userPage/Info' className="dirLink"><li id='2' className="sideBarListItem " onClick={(e) => liCkickFun(e)}><People />Users</li></Link>
-                        <Link to='/userPage' className="dirLink"><li id='3' className="sideBarListItem " onClick={(e) => liCkickFun(e)}><PersonAdd />Vaccinations</li></Link>
-                        <Link to='/userPage' className="dirLink"><li id='3' className="sideBarListItem " onClick={(e) => liCkickFun(e)}><PersonAdd />Add Vaccination</li></Link>
+                        <Link to='/userPage/home' className="dirLink"><li id="1" className="sideBarListItem " onClick={(e) => liCkickFun(e)}><Reorder />Home</li></Link>
+                        <Link to='/userPage/Info' className="dirLink"><li id='2' className="sideBarListItem " onClick={(e) => liCkickFun(e)}><LibraryBooks />UserInfo</li></Link>
+                        <Link to='/userPage/Info' className="dirLink"><li id='2' className="sideBarListItem " onClick={(e) => liCkickFun(e)}><People />Vaccination Lookup</li></Link>
                     </ul>
                     <h3 className="sideBarTitle">Account</h3>
                     <ul className="sideBarList">
--- src/features/UserPage/Userpage.js
+++ src/features/UserPage/Userpage.js
@@ -2,15 +2,22 @@ import React from 'react'
 import Userheader from './Userheader'
 import UserSideBar from './UserSideBar'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import './userPage.css';
+import Home from "./Home";
 
 function Userpage() {
     return (
         <Router>
             <div className="userPage">
                 <Userheader />
-                <UserSideBar />
-                <Switch>
-                </Switch>
+                <div className="userPageMain">
+                    <UserSideBar />
+                    <Switch>
+                        <Route exact path="/userPage/home">
+                            <Home />
+                        </Route>
+                    </Switch>
+                </div>
             </div>
         </Router>
     )
